1674ef3bd4118bff7a76737de5a89be12845760c
Mamun Rana <117065526+programmingmam@users.noreply.github.com>
2023-01-03 23:35:37 -0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+  //scroll up button js//
+const scrollTopButton =document.querySelector('.scroll-top');
+
+scrollTopButton.addEventListener('click', ()=> {
+    window.scrollTo(0,0);
+})
+
+//scroll up button js//
  
